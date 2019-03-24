b5627c2ef722dade5805014bd30d8694b4a1ce2d
daesunsamuelpak <48194410+daesunsamuelpak@users.noreply.github.com>
2019-03-24 17:05:06 -0400

    Delete reverse-solution.rb

--- reverse-solution.rb
+++
@@ -1,8 +0,0 @@
-def reverse(word)
-	reversed = word.reverse
-  	return reversed
-end
-
-puts reverse("cat")          # => "tac"
-puts reverse("programming")  # => "gnimmargorp"
-puts reverse("bootcamp")     # => "pmactoob"
