ab5ad595fd7afba6ebe7c001e133a62483b07e04
daesunsamuelpak <48194410+daesunsamuelpak@users.noreply.github.com>
2019-03-24 16:57:14 -0400

    Add files via upload

---
+++ reverse-solution.rb
@@ -0,0 +1,8 @@
+def reverse(word)
+	reversed = word.reverse
+  	return reversed
+end
+
+puts reverse("cat")          # => "tac"
+puts reverse("programming")  # => "gnimmargorp"
+puts reverse("bootcamp")     # => "pmactoob"
