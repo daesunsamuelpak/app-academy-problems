503ec562582a63985d27a2828627995a864141fe
daesunsamuelpak <48194410+daesunsamuelpak@users.noreply.github.com>
2019-03-24 16:43:17 -0400

    Add files via upload

---
+++ Full Stack Online - Intro to Programming/sum-nums-solution.rb
@@ -0,0 +1,16 @@
+def sum_nums(max)
+	i = 0
+  	count = 0
+  	while i <= max
+    	
+      if i <= max
+      	count += i
+      end
+      
+      i += 1
+    end
+  return count
+end
+
+puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
+puts sum_nums(5) # => 15
